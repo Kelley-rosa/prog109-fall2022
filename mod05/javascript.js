3fb33e651b8d30f8055b50b2ac70b82c2a0848a8
Kelley-rosa <114118224+Kelley-rosa@users.noreply.github.com>
2022-10-20 15:57:30 -0500

    initial push

---
+++ mod05/javascript.js
@@ -0,0 +1,33 @@
+/* Initializing Variables */
+var table = prompt('Enter a number from 0-10');   // Unit of table
+var operator = 'multiplication';                        // Type of calculation
+var i = 1;                                        // Set counter to 1
+var msg = '<h2>Multiplication Table</h2>'         // Message
+
+/* Verifying Variables */
+while (isNaN(table)){
+    table = prompt('INVALID!\n\nEnter a number from 0-10'); 
+}
+
+while (table < 0 || table > 10) {
+    table = prompt('INVALID!\n\nEnter a number from 0-10'); 
+}
+
+/* Calculations For Table */
+if (operator === 'addition') {
+  // Do addition
+  while (i < 11) {
+    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
+    i++;
+  }
+} else {
+  // Do multiplication
+  while (i < 11) {
+    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+    i++;
+  }
+}
+
+// Write the message into the page
+var el = document.getElementById('blackboard');
+el.innerHTML = msg;
