168539d2584dc8e57ca946449daee54cbb0f3177
Kelley-rosa <114118224+Kelley-rosa@users.noreply.github.com>
2022-11-19 17:09:05 -0600

    Update artifact05_javascript.js

--- artifacts/artifact05_javascript.js
+++ artifacts/artifact05_javascript.js
@@ -1,9 +1,9 @@
 var myImages = [
-  "images/img1.jpg", 
-  "images/img2.jpg",
-  "images/img3.jpg",
-  "images/img4.jpg",
-  "images/img5.jpg"];
+  "..images/img1.jpg", 
+  "..images/img2.jpg",
+  "..images/img3.jpg",
+  "..images/img4.jpg",
+  "..images/img5.jpg"];
 
 var captionImages = [
   "Morning Medow",
